123441859b5d222aade13b9fc956974ee99fc5bb
Aryan Bakliwal <106430579+AryanBakliwal@users.noreply.github.com>
2024-10-24 20:49:54 +0530

    django-mongo sample app (#31)
    * sample django mongo app
    
    Signed-off-by: Aryan Bakliwal <aryanbakliwal12345@gmail.com>
    
    * add keploy tests
    
    Signed-off-by: Aryan Bakliwal <106430579+AryanBakliwal@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Aryan Bakliwal <aryanbakliwal12345@gmail.com>
    Signed-off-by: Aryan Bakliwal <106430579+AryanBakliwal@users.noreply.github.com>

---
+++ django-mongo/django_mongo/inventory/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Item
+from .serializers import ItemSerializer
+
+class ItemViewSet(viewsets.ViewSet):
+    
+    def list(self, request):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            item = Item(**serializer.validated_data)
+            item.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        try:
+            item = Item.objects.get(id=pk)
+            serializer = ItemSerializer(item)
+            return Response(serializer.data)
+        except Item.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def update(self, request, pk=None):
+        try:
+            item = Item.objects.get(id=pk)
+            serializer = ItemSerializer(item, data=request.data)
+            if serializer.is_valid():
+                updated_item = serializer.save()  # This will call the update method
+                return Response(ItemSerializer(updated_item).data)  # Serialize the updated item
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Item.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+    def destroy(self, request, pk=None):
+        try:
+            item = Item.objects.get(id=pk)
+            item.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Item.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
