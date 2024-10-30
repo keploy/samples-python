6a203d6c9c8e59b8fcb7382799434425db02c75a
Khushi Trivedi <76205733+trivedi-khushi@users.noreply.github.com>
2024-10-30 18:46:11 +0530

    Update container name in database.py
    Need to change the local host to container name (here postgres) in line-6 in order to start capturing cases using keploy-record command.

--- fastapi-postgres/application/database.py
+++ fastapi-postgres/application/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/studentdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/studentdb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
